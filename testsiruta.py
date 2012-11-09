446094407d8baf157d302e81d4a59d53ce4e26d9
Andrei Cipu <github@strainu.ro>
2012-11-09 22:42:35 +0200

    Adding test file

---
+++ testsiruta.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8  -*-
+
+import sirutalib
+import unittest
+
+class TestSirutaCsv(unittest.TestCase):
+    def setUp(self):
+        self._csv = sirutalib.SirutaCsv()
+        pass
+        
+    def test_get_name(self):
+        pass
+        
+    def test_get_sup_name(self):
+        pass
+        
+    def test_get_sup_code(self):
+        pass
+        
+    def test_get_postal_code(self):
+        pass
+    
+    def test_get_type(self):
+        pass
+        
+    def test_get_county(self):
+        pass
+        
+    def test_get_region(self):
+        pass
+        
+    def test_get_code_by_name(self):
+        pass
+        
+    def test_get_sup_code_by_name(self):
+        pass
+        
+    def test_get_sup_name_by_name(self):
+        pass
+        
+    def test_get_postal_code_by_name(self):
+        pass
+    
+    def test_get_type_by_name(self):
+        pass
+        
+    def test_get_county_by_name(self):
+        pass
+        
+    def test_get_region_by_name(self):
+        pass
+        
+    def test_get_inf_codes(self):
+        pass
+        
+    def test_get_all_counties(self):
+        pass
+    
+    
+if __name__ == '__main__':
+    unittest.main()
