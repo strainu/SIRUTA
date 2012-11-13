4dd7f5ce226e48b5e949049a784ae2d43e8b5afc
Andrei Cipu <acipu@ixiacom.com>
2012-11-13 18:29:05 +0200

    First version of the setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(name='SIRUTAlib',
+      version='1.0',
+      author='Andrei Cipu',
+      author_email='siruta@strainu.ro',
+      url='http://proiecte.strainu.ro/siruta/',
+      license='3-clause BSD',
+      py_modules=['sirutalib'],
+      data_files=[
+          ('.', ['siruta.csv','README.rst']),
+          ('doc/_build/html', ['index.html']),
+          ],
+      )
