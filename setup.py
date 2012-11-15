f9b4a72deaea629da387b8c606fe891d86840aa8
Andrei Cipu <acipu@ixiacom.com>
2012-11-15 13:26:34 +0200

    Satisfactory solution for Issue #8.
    siruta.csv is put in the same folder as the python library.

--- setup.py
+++ setup.py
@@ -1,6 +1,17 @@
 from distutils.core import setup
+import distutils.command.install_data
 import sys
 import os
+
+data_path = os.path.join(sys.prefix, 'sirutalib')
+
+class custom_install_data(distutils.command.install_data.install_data):
+    """need to change self.install_dir to the actual library dir"""
+    def run(self):
+        install_cmd = self.get_finalized_command('install')
+        self.install_dir = getattr(install_cmd, 'install_lib')
+        return distutils.command.install_data.install_data.run(self)
+
 
 setup(name='SIRUTAlib',
       version='1.0',
@@ -10,6 +21,7 @@ setup(name='SIRUTAlib',
       license='3-clause BSD',
       py_modules=['sirutalib'],
       data_files=[
-          (os.path.join(sys.prefix,'sirutalib'), ['siruta.csv','README.rst','INSTALL.rst','doc/help.html']),
+          ('', ['siruta.csv']),
           ],
+      cmdclass = { 'install_data':    custom_install_data },
       )
