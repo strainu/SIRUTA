0078c30fcda452cec1f268ee3ea7d855dbafd05d
Strainu <github@strainu.ro>
2022-01-10 23:51:06 +0000

    Version: upgrade to v1.2
    Signed-off-by: Strainu <github@strainu.ro>

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
+import setuptools
 from distutils.core import setup
 import distutils.command.install_data
+import pathlib
 import sys
 import os
 
 data_path = os.path.join(sys.prefix, 'sirutalib')
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.rst").read_text()
 
 class custom_install_data(distutils.command.install_data.install_data):
     """need to change self.install_dir to the actual library dir"""
@@ -14,10 +22,13 @@ class custom_install_data(distutils.command.install_data.install_data):
         return distutils.command.install_data.install_data.run(self)
 
 
-setup(name='SIRUTAlib',
-      version='1.1',
+setup(name='sirutalib',
+      version='1.2',
       author='Andrei Cipu',
       author_email='siruta@strainu.ro',
+      description="Work with the Romanian settlement database, SIRUTA",
+      long_description=README,
+      long_description_content_type="text/markdown",
       url='http://proiecte.strainu.ro/siruta/',
       license='3-clause BSD',
       py_modules=['sirutalib'],
@@ -25,4 +36,5 @@ setup(name='SIRUTAlib',
           ('', ['siruta.csv', 'README.rst', 'doc/help.html']),
           ],
       cmdclass = { 'install_data':    custom_install_data },
+      setup_requires=['wheel']
       )
